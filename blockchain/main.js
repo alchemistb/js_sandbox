6fa38874a4f9c7c0be5cdcfde331369c5501043a
unknown <alchemistb2018@gmail.com>
2019-04-16 14:56:10 -0400

    [Blockchain JavaScript]

--- blockchain/main.js
+++ blockchain/main.js
@@ -2,6 +2,8 @@
 // https://www.youtube.com/watch?v=zVqczFZr124
 
 
+const SHA256 = require('crypto-js/sha256'); // import SHA256
+
 // Create the Block
 class Block{
   constructor(index, timestamp, data, previousHash = ''){
@@ -9,10 +11,34 @@ class Block{
     this.timestamp = timestamp;
     this.data = data;
     this.previousHash = previousHash;
-    this.hash = '';
+    this.hash = this.calculateHash();
   }
   // Create a method to calculate the hash for each block
   calculateHash(){
-    // INSTALL NPM AND THEN CONTINUE WITH LECTURE
+    return SHA256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data)).toString();
   }
 }
+
+// Create Blockchain
+class Blockchain{
+  constructor(){
+    this.chain = [this.createGenesisBlock()];
+  }
+  createGenesisBlock(){
+    return new Block(0, "4/15/2019", "Genesis", "0");
+  }
+  getLatestBlock(){
+    return this.chain[this.chain.length - 1];
+  }
+  addBlock(newBlock){
+    newBlock.previousHash = this.getLatestBlock().hash;
+    newBlock.hash = newBlock.calculateHash();
+    this.chain.push(newBlock);
+  }
+}
+
+let bgcoin = new Blockchain();
+bgcoin.addBlock(new Block(1, "2/16/1974",{amount: 4}));
+bgcoin.addBlock(new Block(2, "3/16/1974",{amount: 10}));
+
+console.log(JSON.stringify(bgcoin, null,4));
